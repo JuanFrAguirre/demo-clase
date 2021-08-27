a3021b909f068163755d3006132772c04342b807
Juan Francisco Aguirre <jf_aguirre@hotmail.com>
2021-08-27 03:33:14 -0300

    demo-clase-digitalHouse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// primero importamos useEffect y useState de react para poder utilizarlos en nuestro componente
+import { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  // creamos una constante que representa un estado y su setter o modificador (asignador) y le asignamos un valor inicial
+  const [contador, incrementarContador] = useState(0)
+
+  // el useEffect toma dos parametros: el primero es el codigo o 'efecto' que queremos que ejecute, y el segundo (llamado array de dependencias) es quien le dice al useEffect CUANDO debe ejecutar este codigo, segun cual sea la variable por la que este 'escuchando'
+  useEffect(() => {
+    console.log('se produjo un cambio en el contador', contador)
+  }, [contador])
+
+  // en caso de tener un useEffect con el segundo parametro (el array de dependencias) vacio, esto significa que el codigo se ejecutara solo la primera vez que se pinte el componente
+  useEffect(() => {
+    console.log('App.js se acaba de renderizar por primera vez!')
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>[ useState & useEffect ]</h1>
+
+      {/* aqui interpolamos en el html nuestra variable/estado 'contador' para que refleje el valor actual */}
+      <h2>Valor del contador: {contador}</h2>
+
+      {/* en cada uno de los botones asignamos al evento onClick la funcion modificadora/asignadora de nuestro estado 'contador' (el que creamos con el useState) para que al escuchar el clic ejecute el codigo que le pasamos por parametro (por ejemplo que reasigne el valor del contador al valor actual mas uno) */}
+      <button
+        onClick={() => {
+          incrementarContador(contador - 1)
+        }}
+      >
+        - 1
+      </button>
+      <button
+        onClick={() => {
+          incrementarContador(contador + 1)
+        }}
+      >
+        + 1
+      </button>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Ejercicios.js
@@ -0,0 +1,13 @@
+// - Crear un componente Ejercicios
+
+// - Utilizar el hook useState para crear un estado y su funcion modificadora/asignadora (por ej. un contador, un nombre, etc.) y darle el estado inicial correspondiente
+
+// - Utilizar JSX para crear un HTML que permita visualizar el valor actual del estado creado en el punto anterior y algun boton o input que permita modificar ese estado y renderizar el nuevo valor
+
+// - Utilizar el hook useEffect para crear un efecto tal que al renderizar por primera vez el componente se imprima por pantalla algun mensaje
+
+// - Utilizar el hook useEffect para crear otro efecto que dependa del estado creado en el segundo punto e imprima por pantalla el valor correspondiente cada vez que este se modifique
+
+// - Crear un segundo estado, con el hook useState, que sea distinto al primero, asi como tambien el HTML correspondiente como en el punto tres
+
+// - Crear un nuevo efecto con el hook useEffect que dependa del nuevo estado creado en el punto anterior pero que imprima por consola los valores de ambos estados
